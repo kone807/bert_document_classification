1bad0f4369b80f8c2b76085cf081d8f95e261cc9
Hardik Garg <55681128+kone807@users.noreply.github.com>
2022-02-06 15:29:50 +0530

    added mean class

--- examples/ml4health_2019_replication/bert_document_classification/document_bert_architectures.py
+++ examples/ml4health_2019_replication/bert_document_classification/document_bert_architectures.py
@@ -179,6 +179,62 @@ class DocumentBertMaxPool(BertPreTrainedModel):
             if "pooler" in name:
                 param.requires_grad = True
 
+class DocumentBertMean(BertPreTrainedModel):
+    """
+    BERT output over document into linear layer
+    """
+
+    def __init__(self, bert_model_config: BertConfig):
+        super(DocumentBertMean, self).__init__(bert_model_config)
+        self.bert = BertModel(bert_model_config)
+        self.bert_batch_size= self.bert.config.bert_batch_size
+        self.dropout = nn.Dropout(p=bert_model_config.hidden_dropout_prob)
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=bert_model_config.hidden_dropout_prob),
+            nn.Linear(bert_model_config.hidden_size, bert_model_config.num_labels),
+            nn.Tanh()
+        )
+
+    #input_ids, token_type_ids, attention_masks
+    def forward(self, document_batch: torch.Tensor, document_sequence_lengths: list, device='cuda'):
+
+        #contains all BERT sequences
+        #bert should output a (batch_size, num_sequences, bert_hidden_size)
+        bert_output = torch.zeros(size=(document_batch.shape[0],
+                                              min(document_batch.shape[1],self.bert_batch_size),
+                                              self.bert.config.hidden_size), dtype=torch.float, device='cuda')
+
+        #only pass through bert_batch_size numbers of inputs into bert.
+        #this means that we are possibly cutting off the last part of documents.
+
+        for doc_id in range(document_batch.shape[0]):
+            bert_output[doc_id][:self.bert_batch_size] = self.dropout(self.bert(document_batch[doc_id][:self.bert_batch_size,0],
+                                            token_type_ids=document_batch[doc_id][:self.bert_batch_size,1],
+                                            attention_mask=document_batch[doc_id][:self.bert_batch_size,2])[1])
+
+
+        prediction = self.classifier(bert_output.mean(dim=1)[0])
+        assert prediction.shape[0] == document_batch.shape[0]
+        return prediction
+
+    def freeze_bert_encoder(self):
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
+    def unfreeze_bert_encoder(self):
+        for param in self.bert.parameters():
+            param.requires_grad = True
+
+    def unfreeze_bert_encoder_last_layers(self):
+        for name, param in self.bert.named_parameters():
+            if "encoder.layer.11" in name or "pooler" in name:
+                param.requires_grad = True
+    def unfreeze_bert_encoder_pooler_layer(self):
+        for name, param in self.bert.named_parameters():
+            if "pooler" in name:
+                param.requires_grad = True
+
 
 class DocumentBertTransformer(BertPreTrainedModel):
     """
