31ab2c1c8f01fbe29a3c8eb795931a51ed852250
Hardik Garg <55681128+kone807@users.noreply.github.com>
2022-02-08 14:16:35 +0530

    added script for predictions

---
+++ examples/ml4health_2019_replication/smoking_predict.sh
@@ -0,0 +1,13 @@
+pip install transformers
+pip install ConfigArgParse
+
+wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
+tar -xf gdrive_2.1.1_linux_386.tar.gz
+
+./gdrive info 1cjtB74Z4LGLDSUsv_uFzEUWq2j5UqQvj
+
+# +
+./gdrive download 1hdo60po6lO3puuCkFeZ8q7Pqkq1XQN8X
+tar -xvf umls_BERT_docBERTMean.tar.gz 
+
+python ./train_n2c2_2006.py
