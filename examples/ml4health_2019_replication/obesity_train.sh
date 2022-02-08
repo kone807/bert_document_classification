3274f488cb6433789f69083f1e999f50372192d7
Hardik Garg <55681128+kone807@users.noreply.github.com>
2022-02-08 20:58:19 +0530

    Update obesity_train.sh

--- examples/ml4health_2019_replication/obesity_train.sh
+++ examples/ml4health_2019_replication/obesity_train.sh
@@ -5,7 +5,7 @@ wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_l
 tar -xf gdrive_2.1.1_linux_386.tar.gz
 ./gdrive info 1LOAGy_LusCZngqShMHnHH19TwbkJlG7K
 
-python ./train_n2c2_2006.py
+python ./train_n2c2_2008.py
 tar -zcvf ./bioClinicalBert_results.tar.gz ./results
 
 ./gdrive upload ./bioClinicalBert_results.tar.gz --parent 1LOAGy_LusCZngqShMHnHH19TwbkJlG7K
